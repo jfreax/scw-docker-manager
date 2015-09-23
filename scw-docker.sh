7f584a8b1492f5553988c0712512d09701ba77e2
Jens Dieskau <jens@dieskau.pm>
2015-09-23 17:05:47 +0200

    fix spelling error

--- scw-docker.sh
+++ scw-docker.sh
@@ -381,7 +381,7 @@ function update {
   else
     echo "Start emerging..."
     scw exec --gateway=edge hub \
-      "docker exec gentoobuild_genoo-build_1 bash -c \"source ~/.bashrc; emerge-websync; eix-update\""
+      "docker exec gentoobuild_genoo-build_1 bash -c \"source ~/.bashrc; emerge-webrsync; eix-update\""
     scw exec --gateway=edge hub \
       "echo -------- $(date) -------- >> /var/log/emerge-update.log"
     scw exec --gateway=edge hub \
